99fe957a03a1eb785f4eb3eecd1cc78349766cb7
radial80 <40853528+radial80@users.noreply.github.com>
2023-05-10 15:07:32 +0300

    Update EarthquakeManagerModel.swift
    Deleted some unnecessary code lines

--- Earthquake/EarthquakeManagerModel.swift
+++ Earthquake/EarthquakeManagerModel.swift
@@ -1,17 +1,3 @@
-//
-//  WeatherManagerModel.swift
-//  Earthquake
-//
-//  Created by Kasım Sağır on 6.03.2023.
-//{"tarih":"2023.03.07",
-//    "saat":"12:55:54",
-//    "enlem_n":37.709,
-//    "boylam_e":36.5747,"derinlik_km":27.2,
-//    "md":"-.-",
-//"ml":2.2,
-//    "mw":"-.-",
-//"yer":"CINARPINAR-(KAHRAMANMARAS)",
-//    "cozum_niteligi":"Ãlksel"},
 
 import Foundation
 
